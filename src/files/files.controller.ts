e0f091525330506b1f36b0d6aa602e914725f35b
Franco Cabrera <cabrera.franco@outlook.com>
2023-07-25 10:07:34 -0300

    add filter image

---
+++ src/files/files.controller.ts
@@ -0,0 +1,28 @@
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    return file;
+  }
+}
