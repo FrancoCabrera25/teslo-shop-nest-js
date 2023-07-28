3b5cde0eeff7ddd448700f748e274b474329ef40
Franco Cabrera <cabrera.franco@outlook.com>
2023-07-28 16:26:26 -0300

    add auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,64 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createAuthDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createAuthDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      await this.userRepository.save(user);
+
+      delete user.password;
+      return user;
+    } catch (error) {
+      this.handleDbErrors(error);
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    try {
+      const { email, password } = loginUserDto;
+
+      const user = await this.userRepository.findOne({
+        where: { email, password },
+        select: { email: true, password: true },
+      });
+      if (!user) {
+        throw new UnauthorizedException('Credentials are not valid');
+      }
+
+      if (bcrypt.compareSync(password, user.password)) {
+        throw new UnauthorizedException('Credentials are not valid');
+      }
+    } catch (error) {
+      this.handleDbErrors(error);
+    }
+  }
+
+  private handleDbErrors(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs ');
+  }
+}
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
-  findAll() {
-    return `This action returns all files`;
+  getStaticImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+    console.log('path', path);
+    if (!existsSync(path)) {
+      throw new BadRequestException(`no product found with image ${imageName}`);
+    }
+
+    return path;
   }
 }
