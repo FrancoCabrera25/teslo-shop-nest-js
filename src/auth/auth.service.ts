1d0885028dafdee08636033019d238ba303146d0
Franco Cabrera <cabrera.franco@outlook.com>
2023-08-09 23:13:21 -0300

    add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,12 +10,15 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtPayload } from './interface/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
   async create(createAuthDto: CreateUserDto) {
     try {
@@ -28,37 +31,44 @@ export class AuthService {
       await this.userRepository.save(user);
 
       delete user.password;
-      return user;
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email }),
+      };
     } catch (error) {
       this.handleDbErrors(error);
     }
   }
 
   async login(loginUserDto: LoginUserDto) {
-    try {
-      const { email, password } = loginUserDto;
+    const { email, password } = loginUserDto;
 
-      const user = await this.userRepository.findOne({
-        where: { email, password },
-        select: { email: true, password: true },
-      });
-      if (!user) {
-        throw new UnauthorizedException('Credentials are not valid');
-      }
-
-      if (bcrypt.compareSync(password, user.password)) {
-        throw new UnauthorizedException('Credentials are not valid');
-      }
-    } catch (error) {
-      this.handleDbErrors(error);
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid');
     }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+
+    return {
+      ...user,
+      token: this.getJwtToken({ email: user.email }),
+    };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 
   private handleDbErrors(error: any): never {
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
     }
-    console.log(error);
     throw new InternalServerErrorException('Please check server logs ');
   }
 }
