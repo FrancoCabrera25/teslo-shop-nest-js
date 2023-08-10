a250d5a54fe02bc06d6021058debc1dba56d037d
Franco Cabrera <cabrera.franco@outlook.com>
2023-08-10 15:46:12 -0300

    jwt login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { race } from 'rxjs';
+import { RawHeaders } from 'src/common/decorators/raw-headers.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +20,22 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(
+    @GetUser()
+    user: User,
+    @GetUser('email') userEmail,
+    @RawHeaders() rawHeaders,
+  ) {
+    return {
+      ok: true,
+      message: 'Hola mundo private',
+      user,
+      userEmail,
+      rawHeaders,
+    };
   }
 }
