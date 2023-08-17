c21b1babc41b52b7bd4a325bf3c465d681545c5e
Franco Cabrera <cabrera.franco@outlook.com>
2023-08-17 16:27:20 -0300

    on message

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,21 +1,41 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
   constructor(private readonly messageWsService: MessageWsService) {}
 
-  handleConnection(client: Socket, ...args: any[]) {
-    console.log('Client coneect', client.id);
+  handleConnection(client: Socket) {
+    this.messageWsService.registerClient(client);
+
+    this.wss.emit(
+      'clients-updated',
+      this.messageWsService.getConnectedClients(),
+    );
   }
   handleDisconnect(client: Socket) {
-    console.log('Client disconect', client.id);
+    this.messageWsService.removeClient(client.id);
+
+    this.wss.emit(
+      'clients-updated',
+      this.messageWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-form-client')
+  hanldeMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log('client', client.id);
+    console.log('payload', payload);
   }
 }
