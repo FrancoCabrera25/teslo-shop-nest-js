b4a1392833dd1cf8657b4dfcaa67e244b3aba472
Franco Cabrera <cabrera.franco@outlook.com>
2023-08-19 17:57:03 -0300

    web socket

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -8,16 +8,29 @@ import {
 import { MessageWsService } from './message-ws.service';
 import { Socket, Server } from 'socket.io';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interface/jwt-payload.interface';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() wss: Server;
-  constructor(private readonly messageWsService: MessageWsService) {}
+  constructor(
+    private readonly messageWsService: MessageWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  handleConnection(client: Socket) {
-    this.messageWsService.registerClient(client);
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messageWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
 
     this.wss.emit(
       'clients-updated',
@@ -35,7 +48,22 @@ export class MessageWsGateway
 
   @SubscribeMessage('message-form-client')
   hanldeMessageFromClient(client: Socket, payload: NewMessageDto) {
-    console.log('client', client.id);
-    console.log('payload', payload);
+    //emite al client
+    // client.emit('message-form-server', {
+    //   fullName: 'franco',
+    //   message: payload.message || 'no message!!',
+    // });
+
+    //emitir a todos menos al cliente inicial.
+    // client.broadcast.emit('message-form-server', {
+    //   fullName: 'franco',
+    //   message: payload.message || 'no message!!',
+    // });
+
+    //emite a todos
+    this.wss.emit('message-form-server', {
+      fullName: this.messageWsService.getUserFullName(client.id),
+      message: payload.message || 'no message!!',
+    });
   }
 }
